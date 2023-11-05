8caf55fd84ed0706372fa4da623a6b9cc82cac9b
seocoal <jiyoon369@sookmyung.ac.kr>
2023-11-05 20:49:09 +0900

    3-2

--- main.c
+++ main.c
@@ -5,21 +5,28 @@
 
 int main(int argc, char *argv[]) {
 	
-	
-	int grade[N_STUDENT];
-	int score[N_STUDENT];
+
 	int i;
-	printf("input 5 scores: ");
+    int a[N_STUDENT] = {1, 2, 3, 4, 5};
+    int b[N_STUDENT] = {1, 2, 3, 4, 5};
+    int flag=0;
+    
+   
+    for (i=0; i<N_STUDENT; i++)
+	{
 	
+        if (a[i] != b[i])
+        {
+	       printf("array a and b are not same\n");
+	       flag=1;
+	   }
+}
+	   
+	if(flag==0)
+	  printf("array a and b are same\n");
 	
-	for(i=0;i<N_STUDENT; i++)
-	   grade[i]=rand()%100 +1 ;
-	   
-	for (i=0;i<N_STUDENT; i++)
-	   score[i]=grade[i];
-	   
-	for (i=0; i<N_STUDENT; i++)
-	   printf("score[%d]=%d (grade: %i)\n",i, score[i], grade[i]);
+    
+	
 	
 	
 	
