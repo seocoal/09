36ac104e823027d0e69dabd9a6a99ed2534bd016
seocoal <jiyoon369@sookmyung.ac.kr>
2023-11-05 20:25:50 +0900

    2-1

--- main.c
+++ main.c
@@ -7,16 +7,22 @@ int main(int argc, char *argv[]) {
 	
 	
 	int grade[N_STUDENT];
-	int i;
+	int i, average;
+	int sum;
 	printf("input 5 scores: ");
 	
-	for(i=0;i<N_STUDENT; i++)
-	   scanf("%d", &grade[i]);
-	   
+	sum=0;
 	
-	for (i=0; i<N_STUDENT; i++)
-	   printf("grade[%d]=%d\n", i, grade[i]);
+	for(i=0;i<N_STUDENT; i++)
+	{
+		scanf("%d", &grade[i]);
+		sum += grade[i];
+		
+	   }   
 	   
+	average= sum / N_STUDENT;
+	
+	printf("score average: %i\n", average);
 	
 	return 0;
 }
